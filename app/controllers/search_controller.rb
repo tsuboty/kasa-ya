c5fb7a8ae6f34a4defcc79076d7ce18f95930cd7
unknown <tsubota.ryo@SX0000051542-00.g01.fujitsu.local>
2015-02-03 17:24:03 +0900

    umbrella set on map

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,12 @@
 class SearchController < ApplicationController
 	def index
-		@umbrella = Umbrella.all
+		@umbrella = Umbrella.all.to_json
 
+		#武蔵中原
+		@p_lat = 35.581154
+		@p_lon = 139.641474
+		 
 		#render json: @umbrella
-
-
+		@img = ActionController::Base.helpers.asset_path('umbrella_60.png')
 	end
 end
